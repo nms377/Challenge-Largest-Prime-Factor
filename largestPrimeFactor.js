23b8ce16c3b05eabe16327dde6baf36977b66aac
nms377 <nms377@nyu.edu>
2016-11-19 10:32:00 -1000

    Created an if statement to push factors of n into an array. The plan was to sort through the array for prime numbers, then find the largest prime number.

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,19 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
+  var factors = [];
   // do your work here
 
-  return primeNumber;
+//Use modulus to find remainder of 0, if it returns 0 then increase primeNumber by 1 and push number into an array; if it doesn't then increase primeNumber by 1; this returns all factors not just prime
+	if (n % primeNumber === 0){
+		factors.push(primeNumber);
+		primeNumber++;
+
+	}else{
+		primeNumber++;
+	}
+	  return primeNumber;
 };
+
+//Find factors of 600851475143	(used modulus)
+	//Find prime factors	(used if statement)
+		//Sort to find the largest prime factor (use array sort method)
